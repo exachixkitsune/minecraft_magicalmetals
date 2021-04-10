c56b938784b11e4c0afc7d6773fd35188a7598b9
exachixkitsune <exachixkitsune>
2021-04-10 20:16:28 +0100

    Version 1.3.1
    Two Changes:
    Changed the visual name of starmetal to meteoric iron.
    Tags:
      Added forge tags for:
        Ingots
        Ore
        Blocks
      Added items tag for magical focus blocks.

--- src/main/java/net/exachixkitsune/magicalmetals/MagicalMetals.java
+++ src/main/java/net/exachixkitsune/magicalmetals/MagicalMetals.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class MagicalMetals {
 	public static final String MODID = "magicalmetals";
     public static final String MODNAME = "Magical Metals";
-    public static final String VERSION = "1.16.5-1.3";
+    public static final String VERSION = "1.16.5-1.3.1";
 	
     // Constructor
 	public MagicalMetals() {
